a2689f65e8299a19e7bb4f5f96d8450338d763b7
Alex Cruz <acruz@qualitrain.com.mx>
2024-07-17 14:16:58 -0600

    Ejemplos de parseo básico de tokens

--- src/main/java/mx/com/qtx/testjjwt/TokensJwtUtil.java
+++ src/main/java/mx/com/qtx/testjjwt/TokensJwtUtil.java
@@ -1,9 +1,11 @@
 package mx.com.qtx.testjjwt;
 
 import java.security.KeyPair;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
+import java.util.function.Function;
 
 import javax.crypto.SecretKey;
 
@@ -37,12 +39,17 @@ public class TokensJwtUtil {
 	}
 
 	public static String generarToken(String nombreUsuario) {
+		SecretKey llave = generarLlave(AlgoritmoCifradoLlaveSimetrico.HmacSHA256);
+		return generarToken(nombreUsuario, llave);
+	}
+	
+
+	public static String generarToken(String nombreUsuario, SecretKey llave) {
 		long duracionTokensMilis = 1000 * 60 * 60 * 10;
 		
 		String id = UUID.randomUUID()
 				        .toString()
 				        .replace("-", "");
-		SecretKey llave = generarLlave(AlgoritmoCifradoLlaveSimetrico.HmacSHA256);
 		Date ahora = new Date();
 		Date expiracion = new Date(System.currentTimeMillis() + duracionTokensMilis);
 		
@@ -58,9 +65,19 @@ public class TokensJwtUtil {
 	
 	public static String generarToken(String nombreUsuario, Map<String,Object> mapClaims) {
 		SecretKey llave = generarLlave(AlgoritmoCifradoLlaveSimetrico.HmacSHA512);
+		return generarToken(nombreUsuario, mapClaims, llave);
+	}
+	
+	public static String generarToken(String nombreUsuario, Map<String,Object> mapClaims, 
+			                          SecretKey llave) {
 		long duracionTokensMilis = 1000 * 60 * 60 * 10;
+		return generarToken(nombreUsuario,mapClaims,llave,duracionTokensMilis);
+	}
+	
+	public static String generarToken(String nombreUsuario, Map<String,Object> mapClaims, 
+			                          SecretKey llave, long milisDuracion) {
 		Date ahora = new Date();
-		Date expiracion = new Date(System.currentTimeMillis() + duracionTokensMilis);
+		Date expiracion = new Date(System.currentTimeMillis() + milisDuracion);
 		String id = UUID.randomUUID()
 				        .toString()
 				        .replace("-", "");
@@ -76,4 +93,105 @@ public class TokensJwtUtil {
 		              .signWith(llave)
 		              .compact();	
 	}
+	
+	/**
+	 * Usar con tokens firmados
+	 * @param tokenFirmado
+	 * @param llave
+	 * @return
+	 */
+	public static Jws<Claims> extraerJwsClaimsTokenFirmado(String tokenFirmado, SecretKey llave){
+		 JwtParser parser = Jwts.parser()
+				                .verifyWith(llave)
+			                    .build();
+		
+		 Jws<Claims> jwsClaims = parser.parseSignedClaims(tokenFirmado);
+
+		return jwsClaims;
+	}
+	
+	public static Jwt<Header,Claims> extraerJwtTokenSinFirmar(String tokenSinFirma){
+		// Solamente se debe usar con tokens SIN firmar
+		Jwt<Header,Claims> jwtHeaderClaims = Jwts.parser()
+											     .build()
+											     .parseUnsecuredClaims(tokenSinFirma);
+		return jwtHeaderClaims;
+	}
+
+	public static String extraerContenidoTokenFirmadoStr(String tokenFirmado, SecretKey llave) {
+		
+		Jws<Claims> contenidoToken = extraerJwsClaimsTokenFirmado(tokenFirmado, llave);
+		
+		JwsHeader header = contenidoToken.getHeader();
+		String strHeader = "Header:["
+				      + "Algorithm:" + header.getAlgorithm() + ", "
+				      + "CompressionAlgorithm:" + header.getCompressionAlgorithm() + ", "
+				      + "ContentType:" + header.getContentType() + ", "
+				      + "KeyId(protected header):" + header.getKeyId() + ", "
+				      + "Type:" + header.getType() 
+				      + "] ";
+		
+		 Claims payload = contenidoToken.getPayload();
+		 String strPayload = "Payload:["
+				           + "Id:" + payload.getId( )+ ", "
+				           + "Issuer:" + payload.getIssuer() + ", "
+                           + "Subject:" + payload.getSubject() + ", "
+                           + "Expiration:" + payload.getExpiration() + ", "
+                           + "IssuedAt:" + payload.getIssuedAt() + ", "
+                           + "NotBefore:" + payload.getNotBefore()
+     				       + "] ";
+		 
+		 byte[] digest = contenidoToken.getDigest();
+		 int codifIntLlave[] = new int[digest.length];
+		 for(int i=0;i<digest.length;i++) {
+			 codifIntLlave[i] = Byte.toUnsignedInt(digest[i]);
+		 }
+		 String strDigest = "Digest(Signature):" + Arrays.toString(codifIntLlave);
+				 
+		 return strHeader + "\n" + strPayload + "\n" + strDigest;
+	}
+
+	public static String extraerCampoString(String tokenFirmado, SecretKey skLlave, String llaveCampo) {
+		Jws<Claims> contenido = extraerJwsClaimsTokenFirmado(tokenFirmado, skLlave);
+		Claims claims = contenido.getPayload();
+		return (String)claims.get(llaveCampo);
+	}
+
+	//Lanzara excepcion si el token esta vencido o incorrecto
+	public static String extraerUsuarioTokenFirmado(String token, SecretKey skLlave) {
+		Jws<Claims> contenido = extraerJwsClaimsTokenFirmado(token, skLlave);
+		return contenido.getPayload()
+				        .getSubject();
+	}
+	
+	public Date extraerExpiracionTokenFirmado(String token, SecretKey skLlave) {
+		Jws<Claims> contenido = extraerJwsClaimsTokenFirmado(token, skLlave);
+		return contenido.getPayload()
+				        .getExpiration();
+	}
+	
+	//Devuelve el valor del campo, del tipo que sea
+	public static <R> R extraerCampo(String tokenFirmado, Function<Claims, R> getterCampo, SecretKey skLlave) {
+		Jws<Claims> contenido = extraerJwsClaimsTokenFirmado(tokenFirmado, skLlave);
+		Claims claims = contenido.getPayload();
+		return getterCampo.apply(claims);
+	}
+
+	//Devuelve el valor del campo, del tipo que sea
+	public static <R> R extraerCampo(String tokenFirmado, Class<R> tipoJavaCampo, String campo, SecretKey skLlave) {
+		@SuppressWarnings("unchecked")
+		Function<Claims, R> getterCampo = (claims) -> (R) claims.get(campo);
+		return extraerCampo(tokenFirmado, getterCampo, skLlave);
+	}
+	
+	public static boolean tokenFirmadoExpirado(String token, SecretKey skLlave) {
+		try {
+			extraerJwsClaimsTokenFirmado(token, skLlave);
+		}
+		catch(ExpiredJwtException ex) {
+			return true;
+		}
+		return false;
+	}
+	
 }
--- src/main/java/mx/com/qtx/testjjwt/tests/TestJjwt.java
+++ src/main/java/mx/com/qtx/testjjwt/tests/TestJjwt.java
@@ -15,13 +15,18 @@ public class TestJjwt {
 
 	public static void main(String[] args) {
 //		test_GeneracionLlave();
-//		test_CreacionTokenJWT();
-		test_CreacionTokenJWTconMap();
 		
+//		test_CreacionTokenJWT();
+		
+//		test_CreacionTokenJWTconClaimsPersonalizados();
+		
+		test_ParserTokenFirmado();
+		test_ExtraerDatos();
+		test_tokenExpirado();
 	}
 	
 	private static void test_GeneracionLlave() {
-		System.out.println("*** test_GeneracionLlaves() ***");
+		System.out.println("\n*** test_GeneracionLlaves() ***");
 		for(AlgoritmoCifradoLlaveSimetrico algoI:AlgoritmoCifradoLlaveSimetrico.values() ) {
 			
 			SecretKey llave = TokensJwtUtil.generarLlave(algoI);
@@ -52,13 +57,13 @@ public class TestJjwt {
 	}
 	
 	private static void test_CreacionTokenJWT(){
-		System.out.println("*** test_CreacionTokenJWT() ***");
+		System.out.println("\n*** test_CreacionTokenJWT() ***");
 		String token = TokensJwtUtil.generarToken("alex");
 		System.out.println("Token:" + token);		
 	}
 	
-	private static String test_CreacionTokenJWTconMap(){
-		System.out.println("*** test_CreacionTokenJWTconMap() ***");
+	private static String test_CreacionTokenJWTconClaimsPersonalizados(){
+		System.out.println("\n*** test_CreacionTokenJWTconMap() ***");
 		Map<String,Object> mapClaims = new HashMap<>();
 		mapClaims.put("rol","Agente");
 		mapClaims.put("permisos", "0777");
@@ -66,5 +71,76 @@ public class TestJjwt {
 		System.out.println("Token:" + token);
 		return token;
 	}
+	
+	private static void test_ParserTokenFirmado() {
+		System.out.println("\n*** test_ParserTokenFirmado() ***");
+		
+		SecretKey llave = TokensJwtUtil.generarLlave(AlgoritmoCifradoLlaveSimetrico.HmacSHA512);
+		
+		Map<String,Object> mapClaims = new HashMap<>();
+		mapClaims.put("rol","Administrador");
+		mapClaims.put("permisos", "0777");
+		String token = TokensJwtUtil.generarToken("mortadelo", mapClaims, llave);
+		
+		String resultado = TokensJwtUtil.extraerContenidoTokenFirmadoStr(token, llave);
+		System.out.println(resultado);
+		
+		String rol = TokensJwtUtil.extraerCampoString(token, llave, "rol");
+		System.out.println("Rol recuperado del token:" + rol);
+	}
 
+	private static void test_ExtraerDatos() {
+		System.out.println("\n*** test_ExtraerDatos() ***");
+		
+		SecretKey llave = TokensJwtUtil.generarLlave(AlgoritmoCifradoLlaveSimetrico.HmacSHA512);
+		
+		Map<String,Object> mapClaims = new HashMap<>();
+		mapClaims.put("rol","Administrador");
+		mapClaims.put("permisos", "0777");
+		String token = TokensJwtUtil.generarToken("mortadelo", mapClaims, llave);
+		
+		String usuario =  TokensJwtUtil.extraerUsuarioTokenFirmado(token,llave);
+		String rol =      TokensJwtUtil.extraerCampo(token, claims -> (String)claims.get("rol"), llave );
+		String permisos = TokensJwtUtil.extraerCampo(token, claims -> (String)claims.get("permisos"), llave  );
+		System.out.println("Usuario:" + usuario);
+		System.out.println("Rol:" + rol);
+		System.out.println("Permisos:" + permisos);
+		
+		System.out.println("\n..con otra función...");
+		
+		rol =      TokensJwtUtil.extraerCampo(token, String.class , "rol" , llave );
+		permisos = TokensJwtUtil.extraerCampo(token, "permisos".getClass(),"permisos", llave  );
+		System.out.println("Rol:" + rol);
+		System.out.println("Permisos:" + permisos);
+
+	}
+	
+	private static void test_tokenExpirado() {
+		System.out.println("\n*** test_tokenExpirado() ***");
+		
+		SecretKey llave = TokensJwtUtil.generarLlave(AlgoritmoCifradoLlaveSimetrico.HmacSHA512);
+		
+		Map<String,Object> mapClaims = new HashMap<>();
+		mapClaims.put("rol","Administrador");
+		mapClaims.put("permisos", "0777");
+		String tokenOk = TokensJwtUtil.generarToken("mortadelo", mapClaims, llave);
+		String tokenExpirado = TokensJwtUtil.generarToken("filemon", mapClaims, llave, -100);
+		
+		if(TokensJwtUtil.tokenFirmadoExpirado(tokenExpirado, llave)) {
+			System.out.println("El token de filemon está expirado");
+		}
+		else {
+			String usuario = TokensJwtUtil.extraerUsuarioTokenFirmado(tokenExpirado,llave);
+			System.out.println("Todo ok con el token de " + usuario);
+		}
+		
+		if(TokensJwtUtil.tokenFirmadoExpirado(tokenOk, llave)) {
+			System.out.println("El token de mortadelo está expirado");
+		}
+		else {
+			String usuario = TokensJwtUtil.extraerUsuarioTokenFirmado(tokenOk,llave);
+			System.out.println("todo ok con el token de " + usuario);
+		}
+		
+	}	
 }
